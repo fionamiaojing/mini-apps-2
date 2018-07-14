b9ad64211bfc6c1ef4a0d3df96a56a3e39431f51
fiona <fionamiaojing@gmail.com>
2018-07-14 10:11:46 -0700

    add more functionality to the game

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -24,12 +24,36 @@ class App extends React.Component {
         }
     }
 
-    handleClick(event, i, j) {
+    handleClick(i, j) {
         if (this.props.bombBoard[i][j] === 1) {
-            event.target.setAttribute("class", "bomb");
-            setTimeout(() => { alert('BOOOOOM!!!'); }, 200);
+            this.displayBomb();
+            setTimeout(() => { alert('BOOOOOOOOOOOOM'); }, 200);
         } else {
-            this.props.toggle(i, j, this.calculateBomb(i, j));
+            let bombs = this.calculateBomb(i, j);
+            if (bombs === 0) {
+                this.toggleAround(i, j);
+            } else {
+                this.props.toggle(i, j, this.calculateBomb(i, j));
+            }
+            
+            setTimeout(() => {
+                if (this.checkWin()) {
+                    alert('You Win!!!!!!');
+                }
+            }, 200);
+            
+        }
+    }
+
+    displayBomb() {
+        let bombMatrix = this.props.bombBoard;
+        for (let r = 0; r < 10; r++) {
+            for (let c = 0; c < 10; c++) {
+                if (bombMatrix[r][c] === 1) {
+                    document.getElementsByTagName('tr')[r].children[c].setAttribute('class', 'bomb');
+
+                }
+            }
         }
     }
 
@@ -45,15 +69,36 @@ class App extends React.Component {
         return count;
     }
 
+    toggleAround(i, j) {
+        for (let r = Math.max(i - 1, 0); r <= Math.min(i + 1, 9); r++) {
+            for (let c = Math.max(j - 1, 0); c <= Math.min(j + 1, 9); c++) {
+                this.props.toggle(r, c, this.calculateBomb(r, c));
+            }
+        }
+    } 
+
     handleReset() {
         //remove class
-        let ele = document.getElementsByClassName('bomb');
-        ele.length > 0 ? ele[0].classList.remove('bomb') : '';
+        let ele = [...document.getElementsByClassName('bomb')];
+        ele.length > 0 ? ele.map((e) => e.classList.remove('bomb')) : '';
         //clear displayboard
         this.props.resetDisplay();
         //reset bombs
         this.props.resetBomb();
         this.initializeBomb();
+    }
+
+    checkWin() {
+        let currentBoard = this.props.displayBoard;
+        let count = 0;
+        for (let r = 0; r < 10; r++) {
+            for (let c = 0; c < 10; c++) {
+                if (Number.isInteger(currentBoard[r][c])) {
+                    count++;
+                }
+            }
+        }
+        return count === 10 * 9;
     }
 
     render() {
@@ -75,7 +120,7 @@ class App extends React.Component {
                                     {r.map((p, j) => {
                                         return (
                                             <td col={j} key={j}
-                                                onClick={(event) => {this.handleClick(event, i, j);}}
+                                                onClick={() => {this.handleClick(i, j);}}
                                             >
                                                 {p}
                                             </td>
